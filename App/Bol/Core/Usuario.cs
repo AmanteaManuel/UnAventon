717efec860bf8b762422c3d069bc856602ab68ff
AmanteaManuel <amanteamanuel@gmail.com>
2018-05-06 23:39:02 -0300

    se trabajo sobre la bol del proyecto

--- App/Bol/Core/Usuario.cs
+++ App/Bol/Core/Usuario.cs
@@ -8,6 +8,70 @@ namespace Bol
     {
         #region " atributes "
 
+        private string _nombreUsuario;
+        private string _contraseña;
+        private Chofer _chofer;
+        private Pasajero _pasajero;
+
+        #endregion
+
+        #region " Properties "
+
+        /// <summary>
+        /// nombre de usuario en el sistema
+        /// </summary>
+        public string NombreUsuario
+        {
+            get
+            {
+                return _nombreUsuario;
+            }
+            set
+            {
+                _nombreUsuario = value;
+            }
+        }
+
+
+        public Chofer Chofer
+        {
+            get
+            {
+                return _chofer;
+            }
+            set
+            {
+                _chofer = value;
+            }
+        }
+
+        public Pasajero Pasajero
+        {
+            get
+            {
+                return _pasajero;
+            }
+            set
+            {
+                _pasajero = value;
+            }
+        }
+
+        /// <summary>
+        /// Contraseña del usuario
+        /// </summary>
+        public string Contraseña
+        {
+            get
+            {
+                return _contraseña;
+            }
+            set
+            {
+                _contraseña = value;
+            }
+        }
+
         #endregion
     }
 }
--- App/Bol/Core/Vehiculo.cs
+++ App/Bol/Core/Vehiculo.cs
@@ -11,7 +11,11 @@ namespace Bol
         private int _id;
         private int _codigo;
         private string _descripcion;
-
+        private string _color;
+        private string _marca;
+        private string _modelo;
+        private string _patente;
+        private int _asientosDisponibles;        
 
         #endregion
 
@@ -58,9 +62,95 @@ namespace Bol
             }
         }
 
+        /// <summary>
+        /// Color del auto
+        /// </summary>
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+            }
+        }
+
+        /// <summary>
+        /// Marca del auto
+        /// </summary>
+        public string Marca
+        {
+            get
+            {
+                return _marca;
+            }
+            set
+            {
+                _marca = value;
+            }
+        }
+
+        /// <summary>
+        /// Modelo del auto
+        /// </summary>
+        public string Modelo
+        {
+            get
+            {
+                return _modelo;
+            }
+            set
+            {
+                _modelo = value;
+            }
+        }
+
+        /// <summary>
+        /// Patente del auto
+        /// </summary>
+        public string Patente
+        {
+            get
+            {
+                return _patente;
+            }
+            set
+            {
+                _patente = value;
+            }
+        }
+
+        /// <summary>
+        /// Asientos disponibles del auto
+        /// </summary>
+        public int AsientosDisponibles
+        {
+            get
+            {
+                return _asientosDisponibles;
+            }
+            set
+            {
+                _asientosDisponibles = value;
+            }
+        }
+
         #endregion
 
         #region " Views "
+
+        public Vehiculo LoadById(int vehiculoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Vehiculo> GetAllByUsuarioId(int usuarioId)
+        {
+            return null;
+        }
+
         #endregion
     }
 }
