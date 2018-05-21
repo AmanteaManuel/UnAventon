85f69adc375dac9e5ab44bccd02f6583dd5ba176
Gastón Diaz <37700282+Gaston-Diaz@users.noreply.github.com>
2018-05-21 20:08:51 -0300

    Se trabajo sobre el agregar vehiculos.aspx.cs

--- App/UnAventon/Vehiculos/AgregarVehiculo.aspx.cs
+++ App/UnAventon/Vehiculos/AgregarVehiculo.aspx.cs
@@ -16,7 +16,29 @@ namespace UnAventon.Vehiculos
 
         protected void btnAceptar_Click(object sender, EventArgs e)
         {
+            /*try
+            {
+                Validate("CrearVehiculo");
+                if (Page.IsValid)
+                {
+                    Bol.Vehiculo u = new Bol.Vehiculo();
+                    u.Marca = tbMarca.Text;
+                    u.Modelo = tbModelo.Text;
+                    u.Patente = tbPatente.Text;
+                    u.Color = tbColor.Text;
+                    u.Asientos = tbAsientos.Text;
+                    u.SiActivo = true;   //lo dejo porque no se que hace
 
+                    Bol.Vehiculo.Create(u);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Error al agregar el vehiculo", ex);
+            } */
         }
     }
 }
